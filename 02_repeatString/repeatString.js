fd531bcf54d7b89c44e2922310b1f66da5b0769e
Cashal17 <kushtr17@gmail.com>
2024-01-19 02:27:34 -0500

    Completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    let finStr = "";
+    if (count < 0) return 'ERROR';
+    for (let i = count; i > 0; i--) {
+        finStr += word;
+    }   
+    return finStr;
 };
 
 // Do not edit below this line
