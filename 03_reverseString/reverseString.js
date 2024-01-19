fab90e304e224657f499294d96cd24d63f879664
Cashal17 <kushtr17@gmail.com>
2024-01-19 03:04:41 -0500

    Completed 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArr = str.split('');
+    let left = 0;
+    let right = str.length - 1;
+    while (left < right) {
+        let temp = strArr[left];
+        strArr[left] = strArr[right];
+        strArr[right] = temp;
+        left++; right--;
+    }
+    return strArr.join('');
 };
 
 // Do not edit below this line
