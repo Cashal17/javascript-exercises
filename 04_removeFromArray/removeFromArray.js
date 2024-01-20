2db3536f4cf52a0071e4478cb1a32a819d110037
Cashal17 <kushtr17@gmail.com>
2024-01-19 21:54:33 -0500

    Completed 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let finArr = [];
+    arr.forEach(element => {
+        if (!args.includes(element))
+            finArr.push(element);
+    });
+    return finArr;
 };
 
 // Do not edit below this line
