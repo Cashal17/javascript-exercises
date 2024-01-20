4796ed98939f58a331969d4aeb68928591f30d4b
Cashal17 <kushtr17@gmail.com>
2024-01-20 00:06:17 -0500

    Completed 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  return Math.round((5.0/9) * (fahren-32) * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * (9.0/5)) + 32) * 10)/10;
 };
 
 // Do not edit below this line
