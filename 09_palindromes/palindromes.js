d35c71aee12296547360b1163f6ac1e5716104fa
Cashal17 <kushtr17@gmail.com>
2024-01-20 02:27:29 -0500

    Completed 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strArr = str.toLowerCase().split('');
+    strArr = strArr.filter(
+        (elem) => (elem.charCodeAt(0) >= 'a'.charCodeAt(0) && elem.charCodeAt(0) <= 'z'.charCodeAt(0)) ||
+        (elem.charCodeAt(0) >= '0'.charCodeAt(0) && elem.charCodeAt(0) <= '9'.charCodeAt(0))
+    );
+    let left = 0; let right = strArr.length -1;
+    while (left < right) {
+        if (strArr[left] !== strArr[right])
+            return false;
+        left++; right--;
+    }
+    return true;
 };
 
 // Do not edit below this line
