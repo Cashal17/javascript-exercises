23cd1bcea324f4d0c384455c1c8a6d2ecc273666
Cashal17 <kushtr17@gmail.com>
2024-01-20 00:42:23 -0500

    Completed 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => acc + curr, 0); 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	if (exp === 0)
+    return 1;
+  let ans = num;
+  for (let i = 1; i < exp; i++) 
+    ans *= num;
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  let ans = num;
+  for (let i = num-1; i > 0; i--) {
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
